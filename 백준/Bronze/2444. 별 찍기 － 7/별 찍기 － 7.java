2715c86944e4b432cdd632d8c93c32a16aea2dfd
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-01-12 15:41:35 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 444 ms, Memory: 21816 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i < N + 1 ; i++) {
+            for (int j = i; j < N; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = N - 1; i > 0 ; i--) {
+            for (int j = i; j < N; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < 2 * i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
