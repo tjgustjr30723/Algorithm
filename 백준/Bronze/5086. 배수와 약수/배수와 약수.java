49706f88c300bf8bfe12784978d18380c8bbb7af
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-01-25 16:41:03 +0900

    [Bronze III] Title: 배수와 약수, Time: 172 ms, Memory: 17260 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            if (A == 0 && B == 0){
+                break;
+            }
+            if (B % A == 0) {
+                System.out.println("factor");
+            } else if (A % B == 0) {
+                System.out.println("multiple");
+            } else {
+                System.out.println("neither");
+            }
+        }
+    }
+}
