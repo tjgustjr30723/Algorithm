f34d088a9a2733e987eb479dc726b02a7bac9768
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-03-06 16:34:25 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -1,24 +1,22 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a1 = sc.nextInt();
-        int a0 = sc.nextInt();
-        int c = sc.nextInt();
-        int n0 = sc.nextInt();
-        boolean result = false;
-        if (c >= a1) {
-            if (a1 == c) {
-                result = (0 >= a0);
-            } else if (a0 == 0) {
-                result = true;
-            } else if (a1 == 0) {
-                result = ((c * n0) >= a0);
-            } else result = (n0 >= (a0 / (c - a1)));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
+
+        if (a1*n0 + a0 <= c*n0 && c >= a1) { 
+            bw.write("1");
+        } else {
+            bw.write("0");
         }
-        if (result){
-            System.out.println(1);
-        }else System.out.println(0);
+        bw.flush();
+        bw.close();
     }
 }
