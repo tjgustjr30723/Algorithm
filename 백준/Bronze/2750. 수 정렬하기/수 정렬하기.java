15e8dcab03ee617879bbd0b3e1291c28c0e2fb70
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-03-06 15:46:45 +0900

    [Bronze II] Title: 수 정렬하기, Time: 112 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -8,11 +8,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        Set<Integer> set = new TreeSet<>();
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            set.add(Integer.parseInt(br.readLine()));
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        for (Integer s : set) {
+        Arrays.sort(arr);
+        for (int s : arr) {
             sb.append(s).append("\n");
         }
         System.out.println(sb);
