3e1e2eaae164d2a24bd37d300c5efdd550111722
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-03-08 12:31:31 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1548 ms, Memory: 336908 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -3,25 +3,22 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[10001];
         int N = Integer.parseInt(br.readLine());
-        int[] arr1 = new int[N];
-        int[] count = new int[10_000];
-        int[] result = new int[N];
+        
         for (int i = 0; i < N; i++) {
-            arr1[i] = Integer.parseInt(br.readLine());
-            count[arr1[i] - 1]++;
+            arr[Integer.parseInt(br.readLine())]++;
         }
-        for (int i = 1; i < 10_000;i++) {
-            count[i] += count[i - 1];
+        br.close();
+
+        for (int i = 1; i < 10001; i++) {
+            while (arr[i] > 0) {
+                sb.append(i).append("\n");
+                arr[i]--;
+            }
         }
-        for (int i = N - 1; i >= 0; i--) {
-            count[arr1[i] - 1]--;
-            result[count[arr1[i] - 1]] = arr1[i];
-        }
-        for (int i : result) {
-            bw.write(i + "\n");
-        }
-        bw.flush();
+        System.out.println(sb);
     }
 }
