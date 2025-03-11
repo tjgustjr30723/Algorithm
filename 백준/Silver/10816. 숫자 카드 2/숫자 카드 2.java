f55bc3b68882f55d774e918019c5cf44598af256
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-03-11 14:38:46 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 884 ms, Memory: 149476 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -11,17 +11,13 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             String num1 = st.nextToken();
-            if (!map.containsKey(num1)) {
-                map.put(num1, 0);
-            } else map.put(num1, map.get(num1) + 1);
+            map.put(num1, map.getOrDefault(num1, 0) + 1);
         }
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
             String num2 = st.nextToken();
-            if (map.containsKey(num2)) {
-                sb.append(map.get(num2) + 1).append(" ");
-            } else sb.append(0).append(" ");
+            sb.append(map.getOrDefault(num2, 0)).append(" ");
         }
         System.out.println(sb);
     }
