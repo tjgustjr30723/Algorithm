37edac08191b0d0c5122e6a4a74b4eb4c9b68ee6
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-03-25 14:49:54 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 168 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt(); 
+        int V = sc.nextInt();  
+
+        int day = (V - B) / (A - B);
+
+        if ((V - B) % (A - B) != 0) {
+            day++;
+        }
+
+        System.out.println(day);
+    }
+}
