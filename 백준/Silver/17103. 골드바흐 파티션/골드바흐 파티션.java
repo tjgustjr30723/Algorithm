bba203cfc92d03622e36fc0988cab1fd35383509
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-04-03 17:20:24 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 224 ms, Memory: 16120 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -1,22 +1,15 @@
 import java.io.*;
 
 public class Main {
+    public static boolean[] prime = new boolean[1000001];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        isPrime();
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
             int count = 0;
-            boolean[] prime = new boolean[n + 1];
-            for (int j = 2; j <= n; j++) {
-                if (prime[j]) {
-                    continue;
-                }
-                for (int k = j + j; k <= n; k += j) {
-                    prime[k] = true;
-                }
-            }
             for (int j = 2; j <= n / 2; j++) {
                 if (!prime[j] && !prime[n - j]) {
                     count++;
@@ -25,5 +18,16 @@ public class Main {
             sb.append(count).append("\n");
         }
         System.out.println(sb);
+    }
+    private static void isPrime() {
+        prime[0] = prime[1] = true;
+        for (int j = 2; j <= Math.sqrt(prime.length - 1); j++) {
+            if (prime[j]) {
+                continue;
+            }
+            for (int k = j + j; k < prime.length; k += j) {
+                prime[k] = true;
+            }
+        }
     }
 }
