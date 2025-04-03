0f97cac1c697b93d9301431a9933f57aeef4ed37
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-04-03 17:06:23 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 496 ms, Memory: 73600 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,7 +7,6 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
-            List<Integer> list = new ArrayList<>();
             int count = 0;
             boolean[] prime = new boolean[n + 1];
             for (int j = 2; j <= n; j++) {
@@ -19,15 +16,12 @@ public class Main {
                 for (int k = j + j; k <= n; k += j) {
                     prime[k] = true;
                 }
-                list.add(j);
             }
-            for (int j = 0; j < list.size(); j++) {
-                int index = list.get(j);
-                if (!prime[n - index] && (n - index) > index || (n - index) == index) {
+            for (int j = 2; j <= n / 2; j++) {
+                if (!prime[j] && !prime[n - j]) {
                     count++;
                 }
             }
-            list.clear();
             sb.append(count).append("\n");
         }
         System.out.println(sb);
