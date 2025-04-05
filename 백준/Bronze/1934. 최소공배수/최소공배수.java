b1f64a85057ccd4170082e253cef80d13d5c3492
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-04-05 14:22:48 +0900

    [Bronze I] Title: 최소공배수, Time: 124 ms, Memory: 14628 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,28 +1,27 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-        int A = 0, B = 0;
-        int[] arr = new int[T];
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
-            A = Integer.parseInt(st.nextToken());
-            B = Integer.parseInt(st.nextToken());
-            arr[i] = A * B / gcd(A, B);
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i : arr) {
-            sb.append(i).append("\n");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int result = (A * B) / isPrime(A,B);
+            sb.append(result).append("\n");
         }
         System.out.println(sb);
+
     }
-    private static int gcd(int a, int b) {
+
+    private static int isPrime(int a, int b) {
+        int r;
         while (b != 0) {
-            int r = a % b;
+            r = a % b;
             a = b;
             b = r;
         }
