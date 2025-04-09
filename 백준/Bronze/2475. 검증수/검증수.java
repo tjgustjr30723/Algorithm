ce1232340aae8ad4f318a32c79b99799254d9c9b
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-04-09 18:27:07 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        
+        int result = a * a + b * b + c * c + d * d + e * e;
+        System.out.println(result % 10);
+    }
+}
