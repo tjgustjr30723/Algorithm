f728ffc604a25873de9484edc6d804e96b38e194
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-07-17 18:20:55 +0900

    [Bronze III] Title: 알람 시계, Time: 120 ms, Memory: 16180 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,31 +1,20 @@
 import java.util.*;
+import java.io.*;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Clock clock = new Clock();
-        int hour = scanner.nextInt();
-        int minute = scanner.nextInt();
-        clock.setting(hour, minute);
-    }
-}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-class Clock {
-    private int Hour;
-    private int Minute;
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-    public void setting(int hour, int minute) {
-        if (minute < 45) {
-            if (hour == 0){
-                Hour = 23;
-            }
-            else Hour = hour - 1;
-            Minute = minute + 60 - 45;
-        }
-        else {
-            Hour = hour;
-            Minute = minute - 45;
-        }
-        System.out.println(Hour + " " + Minute);
+        if (M < 45) {
+            if (H == 0) {
+                H = 23;
+            } else H -= 1;
+            M += 15;
+        } else M -= 45;
+        System.out.println(H + " " + M);
     }
 }
