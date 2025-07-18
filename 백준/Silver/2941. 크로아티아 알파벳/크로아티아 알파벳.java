82958650bf4af80b29ebdbb71d90fa4490e4a22c
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-07-18 17:49:51 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,39 +1,28 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        Study study = new Study(s);
-        study.check();
-    }
-}
-class Study {
-    private String s;
-
-    public Study(String s) {
-        this.s = s;
-    }
-
-    public void check() {
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (i > 1 && s.charAt(i) == '=' && s.charAt(i - 1) == 'z' && s.charAt(i - 2) == 'd') {
-                count--;
-                continue;
-            }
-            if (i > 0 && s.charAt(i) == '=') {
-                if (s.charAt(i - 1) == 'c' || s.charAt(i - 1) == 's' || s.charAt(i - 1) == 'z') {
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            char c = s.charAt(i);
+            char d = s.charAt(i - 1);
+            if (c == '=') {
+                if (i >= 2 && (d == 'z' && s.charAt(i - 2) == 'd')) {
+                    count -= 1;
+                    continue;
+                } else if (d == 'c' || d == 's' || d == 'z') {
                     continue;
                 }
-            }
-            if (i > 0 && s.charAt(i) == '-') {
-                if (s.charAt(i-1) == 'c' || s.charAt(i - 1) == 'd'){
+            } else if (c == '-') {
+                if (d == 'd' || d == 'c') {
                     continue;
                 }
-            }
-            if (i > 0 && s.charAt(i) == 'j') {
-                if (s.charAt(i-1) == 'l' || s.charAt(i - 1) == 'n'){
+            } else if (c == 'j') {
+                if (d == 'l' || d == 'n') {
                     continue;
                 }
             }
