89564d353ee4236840b0fd707f059a6f3c93c411
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-07-18 18:27:29 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,25 +1,32 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        String[] str = new String[N];
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
         for (int i = 0; i < N; i++) {
-            str[i] = sc.next();
-        }
-        checker(str);
-    }
-    private static void checker(String[] str) {
-        int count = str.length;
-        for (int i = 0; i < str.length; i++) {
-            for (int j = 0; j < str[i].length() - 1; j++) {
-                if (str[i].charAt(j) != str[i].charAt(j + 1)) {
-                    if (str[i].indexOf(str[i].charAt(j) , j + 1) > 0) {
-                        count--;
+            String s = br.readLine();
+            List<Character> list = new ArrayList<>();
+            char ch = s.charAt(0);
+            list.add(ch);
+            boolean flag = true;
+            for (int j = 1; j < s.length(); j++) {
+                if (s.charAt(j) != ch){
+                    if (list.contains(s.charAt(j))){
+                        flag = false;
                         break;
                     }
+                    ch = s.charAt(j);
+                    list.add(ch);
                 }
+            }
+            if (flag){
+                count++;
             }
         }
         System.out.println(count);
