587a61a1ce7b3ac139d5d1d4aa9f36b94c27ae94
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-07-23 14:39:51 +0900

    [Bronze III] Title: 배수와 약수, Time: 100 ms, Memory: 13984 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,21 +1,30 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while(true){
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            if (A == 0 && B == 0){
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int N, M;
+
+        do {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+            if (N == 0 || M == 0) {
                 break;
             }
-            if (B % A == 0) {
-                System.out.println("factor");
-            } else if (A % B == 0) {
-                System.out.println("multiple");
-            } else {
-                System.out.println("neither");
-            }
-        }
+            if (M % N == 0) {
+                sb.append("factor").append('\n');
+            } else if (N % M == 0) {
+                sb.append("multiple").append('\n');
+            } else
+                sb.append("neither").append('\n');
+        } while (true);
+        System.out.println(sb);
     }
 }
