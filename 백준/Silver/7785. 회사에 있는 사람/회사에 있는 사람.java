958a477ff93cdad811c1d3355b0b278a98364458
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-09-04 17:26:15 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 632 ms, Memory: 50496 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,24 +1,33 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
         StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
         Map<String, String> map = new HashMap<>();
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
-            map.put(name, st.nextToken());
-        }
-        List<String> list = new ArrayList<>(map.keySet());
-        list.sort(Collections.reverseOrder());
-        for (String s : list) {
-            if (map.get(s).equals("enter")) {
-                sb.append(s).append("\n");
+            String report = st.nextToken();
+            if (report.equals("leave")) {
+                map.remove(name);
+                continue;
             }
+            map.put(name, report);
+        }
+        Set<String> keys = map.keySet();
+        List<String> keyList = new ArrayList<>(keys);
+        Collections.sort(keyList, Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+        for (String key : keyList) {
+            sb.append(key).append("\n");
         }
         System.out.println(sb);
     }
