4a17a893de2eeba7c0a20f370474a3f5a28c2abd
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-09-16 16:09:18 +0900

    [Bronze I] Title: 최소공배수, Time: 116 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -12,21 +12,18 @@ class Main {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            sb.append((A * B) / cul(A, B)).append("\n");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a * b / gcd(a,b)).append("\n");
         }
         System.out.println(sb);
     }
-
-    private static Integer cul(int A, int B) {
-        int r = A % B;
-        while (r != 0) {
-            A = B;
-            B = r;
-            r = A % B;
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
         }
-        return B;
+        return a;
     }
-
 }
