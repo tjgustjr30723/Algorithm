c76af77f01413b73a4382e116d31ea91ac63d51f
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-09-22 23:07:49 +0900

    [Silver IV] Title: 괄호, Time: 100 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -6,33 +6,34 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine()); //명령어의 수
-
-        Deque<Character> stack = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < T; i++) {
-            stack.clear();
-            boolean flag = true;
-            String s = br.readLine();
-            for (int j = 0; j < s.length(); j++) {
-                char c = s.charAt(j);
-                if (c == '(') {
-                    stack.push(c);
-                } else if (c == ')') {
-                    if (stack.isEmpty()) {
-                        flag = false;
-                        break;
-                    }
-                    stack.pop();
-                }
+            sb.append(solve(br.readLine())).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+    
+    public static String solve(String input) {
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            
+            if (c == '('){
+                stack.push(c);
             }
-            if (!flag) {
-                sb.append("NO").append("\n");
-            } else if (!stack.isEmpty()) {
-                sb.append("NO").append("\n");
-            } else {
-                sb.append("YES").append("\n");
+            else if (stack.isEmpty()){
+                return "NO";
+            }
+            else {
+                stack.pop();
             }
         }
-        System.out.println(sb);
+        
+        if (stack.isEmpty()){
+            return "YES";
+        } else return "NO";
     }
 }
