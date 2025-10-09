d76f37775628efdf696b2908a7713791f9850e57
seo hyun seok <76424127+tjgustjr30723@users.noreply.github.com>
2025-10-09 12:17:09 +0900

    [Bronze IV] Title: 베라의 패션, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/15439. 베라의 패션/베라의 패션.java
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -1,9 +1,10 @@
-import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        System.out.println(N * (N - 1));
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N * (N-1));
     }
 }
+
